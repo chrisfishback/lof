9388e47558bc1a17249c7f09f6a1337f97e9069d
Chris Fishback <chrisfishback@Mac.attlocal.net>
2025-03-12 17:49:43 -0500

    fix and add necessary restrictions

--- src/components/clients/TeamsClient.ts
+++ src/components/clients/TeamsClient.ts
@@ -2,37 +2,40 @@ import {Team} from "../types/Team.ts";
 import {Player} from "../types/Player.ts";
 import { Client, Databases} from "appwrite"
 
-export const DATABASE_ID = "67cc7d69001be23d37c5";
-export const COLLECTION_ID = "67cc7e000032407612e9";
-export const PROJECT_ID = "67cc78df0039c5fd5d2e"
+export const DATABASE_ID = process.env.APPWRITE_LOF_PROJECT_ID;
+export const COLLECTION_ID = process.env.APPWRITE_LOF_DB_ID;
+export const PROJECT_ID = process.env.APPWRITE_TEAM_COLLECTION_ID;
 
 export const getAllTeams = async (): Promise<Team[]> => {
-    const client = new Client()
-        .setEndpoint('https://cloud.appwrite.io/v1')
-        .setProject(PROJECT_ID);
+    if(DATABASE_ID && COLLECTION_ID && PROJECT_ID) {
+        const client = new Client()
+            .setEndpoint('https://cloud.appwrite.io/v1')
+            .setProject(PROJECT_ID);
 
-    const databases = new Databases(client);
+        const databases = new Databases(client);
 
-    const response = await databases.listDocuments(
-        DATABASE_ID,
-        COLLECTION_ID,
-    );
+        const response = await databases.listDocuments(
+            DATABASE_ID,
+            COLLECTION_ID,
+        );
 
-    console.log("response")
-    console.log(response)
+        console.log("response")
+        console.log(response)
 
-    const teams: Team[] = response.documents.map((doc) => ({
-        name: doc.name,
-        wins: doc.wins,
-        losses: doc.losses,
-        ties: doc.ties,
-        players: doc.players.map((player: any) => ({
-            name: player.name
-        } as Player))
-    } as Team));
+        const teams: Team[] = response.documents.map((doc) => ({
+            name: doc.name,
+            wins: doc.wins,
+            losses: doc.losses,
+            ties: doc.ties,
+            players: doc.players.map((player: any) => ({
+                name: player.name
+            } as Player))
+        } as Team));
 
-    console.log("teams")
-    console.log(teams)
-    
-    return teams;
+        console.log("teams")
+        console.log(teams)
+        
+        return teams;
+    }
+    return [] as Team[];
 }
