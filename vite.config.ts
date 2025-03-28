c90350e03d17793cfdcec6109b192660432312b5
Chris Fishback <chrisfishback@Chriss-MacBook-Pro.local>
2025-03-28 08:56:37 -0500

    update configs for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/lof/',
   plugins: [react()],
   test: {
     globals: true,
@@ -11,8 +11,5 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    proxy: {
-      '/api': 'http://locahost:8080'
-    }
   }
 })
