429031dd351af02ae156e078bf604c39a36dc639
Chris Fishback <chrisfishback@Mac.attlocal.net>
2025-04-03 07:07:46 -0500

    add correct files

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+    appwrite: {
+      projectId: import.meta.env.VITE_APPWRITE_LOF_PROJECT_ID,
+      databaseId: import.meta.env.VITE_APPWRITE_LOF_DB_ID,
+      collectionId: import.meta.env.VITE_APPWRITE_TEAM_COLLECTION_ID
+    }
+  };
---
+++ src/lib/appwrite.ts
@@ -0,0 +1,13 @@
+import { Client, Databases } from "appwrite";
+
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_LOF_PROJECT_ID;
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_LOF_DB_ID;
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_TEAM_COLLECTION_ID;
+
+const client = new Client()
+  .setEndpoint('https://cloud.appwrite.io/v1')
+  .setProject(PROJECT_ID || '');
+
+const databases = new Databases(client);
+
+export { client, databases, PROJECT_ID, DATABASE_ID, COLLECTION_ID };
