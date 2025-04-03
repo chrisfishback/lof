7ff904daf1097af5a0dee8c31dab185079b799e3
Chris Fishback <chrisfishback@Mac.attlocal.net>
2025-04-03 07:20:17 -0500

    log2

--- src/components/landing-page/Content.tsx
+++ src/components/landing-page/Content.tsx
@@ -28,6 +28,12 @@ const Content = () => {
 
     useEffect(() => {
         getAllTeams().then(retrievedTeams => setTeams(retrievedTeams))
+
+        console.log('Environment variables:', {
+            projectId: import.meta.env.VITE_APPWRITE_LOF_PROJECT_ID,
+            databaseId: import.meta.env.VITE_APPWRITE_LOF_DB_ID,
+            collectionId: import.meta.env.VITE_APPWRITE_TEAM_COLLECTION_ID
+          });
     }, []);
 
     return (
