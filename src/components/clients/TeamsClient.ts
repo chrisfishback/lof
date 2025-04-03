74d2e45b0446d864040290232270dad2751a258a
Chris Fishback <chrisfishback@Mac.attlocal.net>
2025-04-03 07:02:37 -0500

    update and improve appwrite implementation

--- src/components/clients/TeamsClient.ts
+++ src/components/clients/TeamsClient.ts
@@ -1,42 +1,30 @@
-import {Team} from "../types/Team.ts";
-import {Player} from "../types/Player.ts";
-import { Client, Databases } from "appwrite"
-
-export const PROJECT_ID = import.meta.env.VITE_APPWRITE_LOF_PROJECT_ID;
-export const DATABASE_ID = import.meta.env.VITE_APPWRITE_LOF_DB_ID;
-export const COLLECTION_ID = import.meta.env.VITE_APPWRITE_TEAM_COLLECTION_ID;
+import { Team } from "../types/Team.ts";
+import { Player } from "../types/Player.ts";
+import { databases, DATABASE_ID, COLLECTION_ID } from "../../lib/appwrite";
 
 export const getAllTeams = async (): Promise<Team[]> => {
+    if (DATABASE_ID && COLLECTION_ID) {
+        try {
+            const response = await databases.listDocuments(
+                DATABASE_ID,
+                COLLECTION_ID,
+            );
 
-    if(DATABASE_ID && COLLECTION_ID && PROJECT_ID) {
-        const client = new Client()
-            .setEndpoint('https://cloud.appwrite.io/v1')
-            .setProject(PROJECT_ID);
-
-        const databases = new Databases(client);
-
-        const response = await databases.listDocuments(
-            DATABASE_ID,
-            COLLECTION_ID,
-        );
-
-        // console.log("response")
-        // console.log(response)
-
-        const teams: Team[] = response.documents.map((doc) => ({
-            name: doc.name,
-            wins: doc.wins,
-            losses: doc.losses,
-            ties: doc.ties,
-            players: doc.players.map((player: any) => ({
-                name: player.name
-            } as Player))
-        } as Team));
-
-        // console.log("teams")
-        // console.log(teams)
-        
-        return teams;
+            const teams: Team[] = response.documents.map((doc) => ({
+                name: doc.name,
+                wins: doc.wins,
+                losses: doc.losses,
+                ties: doc.ties,
+                players: doc.players.map((player: any) => ({
+                    name: player.name
+                } as Player))
+            } as Team));
+            
+            return teams;
+        } catch (error) {
+            console.error("Error fetching teams:", error);
+            return [];
+        }
     }
     return [] as Team[];
-}
+};
