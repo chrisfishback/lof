ae28f30f04bd264986d62d44b53fd8c53d8d314b
Chris Fishback <chrisfishback@Mac.attlocal.net>
2025-04-03 07:25:58 -0500

    push secrets

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -4,16 +4,10 @@ const PROJECT_ID = import.meta.env.VITE_APPWRITE_LOF_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_LOF_DB_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_TEAM_COLLECTION_ID;
 
-console.log('Environment variables:', {
-    projectId: import.meta.env.VITE_APPWRITE_LOF_PROJECT_ID,
-    databaseId: import.meta.env.VITE_APPWRITE_LOF_DB_ID,
-    collectionId: import.meta.env.VITE_APPWRITE_TEAM_COLLECTION_ID
-  });
-
 const client = new Client()
   .setEndpoint('https://cloud.appwrite.io/v1')
   .setProject(PROJECT_ID || '');
 
-const databases = new Databases(client);
+const databases = new Databases(client); //code to allow push lol
 
 export { client, databases, PROJECT_ID, DATABASE_ID, COLLECTION_ID };
