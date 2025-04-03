bcb916475a5f7242bf54d570e86f79f908064993
Chris Fishback <chrisfishback@Chriss-MacBook-Pro.local>
2025-04-03 08:21:49 -0500

    update vite env in src config

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const config = {
     appwrite: {
-      projectId: import.meta.env.VITE_APPWRITE_LOF_PROJECT_ID,
-      databaseId: import.meta.env.VITE_APPWRITE_LOF_DB_ID,
-      collectionId: import.meta.env.VITE_APPWRITE_TEAM_COLLECTION_ID
+      projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+      databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+      collectionId: import.meta.env.VITE_APPWRITE_COLLECTION_ID
     }
   };
